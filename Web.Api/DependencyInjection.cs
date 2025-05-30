7930aace45926c66cf258093d1c1f1738e15934e
Scott Mains <scottmains@hotmail.co.uk>
2025-05-30 10:39:16 +0100

    fixes

--- Web.Api/DependencyInjection.cs
+++ Web.Api/DependencyInjection.cs
@@ -9,7 +9,7 @@ public static class DependencyInjection
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
 
-
+        services.AddMemoryCache();
         services.AddExceptionHandler<GlobalExceptionHandler>();
         services.AddProblemDetails();
 
